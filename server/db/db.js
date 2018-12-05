d643ab7c96769c9cb24a1594ff8cb9b7134281f3
Linda Morales <lindagmorales94@gmail.com>
2018-12-05 15:00:56 -0800

    fixed heroku database connection

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,11 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('postgres://localhost:5432/TTPFullstackAssessment', {
-  logging: false,
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL ||
+    'postgres://localhost:5432/TTPFullstackAssessment',
+  {
+    logging: false,
+  }
+);
 
 module.exports = db;
